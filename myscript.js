8710ce40f735504f573f927b1ef5654f98e80205
InvisionWeb <37048390+invisionweb@users.noreply.github.com>
2025-01-27 18:39:09 +0530

    Add random number generation and user guessing game to myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,29 @@
 console.log("Testing myscript.js")
+
+//let myName = prompt("What is your name?")
+//alert("Hello " + myName + " Length of your name is " + myName.length)
+
+let randomNumber = Math.random()
+console.log(randomNumber)
+
+// generate random number between 1 and 6
+let diceRoll = Math.floor(Math.random() * 6) + 1
+console.log("diceRoll " + diceRoll)
+
+let getNumber = parseInt(prompt("Enter a number"))
+
+if(diceRoll === getNumber){
+    console.log("You guessed the right number")
+    document.write("You guessed the right number")
+}
+else{
+    console.log("You guessed the wrong number")
+    document.write("You guessed the wrong number")
+}
+
+/* if(diceRoll === 1 || diceRoll === 2){
+    //console.log("You rolled a 1 or 2")
+}
+else{
+
+} */
