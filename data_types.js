3cf59384282ff0485d7ba05e38534368fb54c2f7
InvisionWeb <37048390+invisionweb@users.noreply.github.com>
2025-03-31 19:21:45 +0530

    functions

---
+++ data_types.js
@@ -0,0 +1,14 @@
+let a = 5
+console.log(a)
+
+let password = "xyzabc"
+console.log('length of password is ', password.length)
+
+if(password.length <= 6)
+{
+    console.log('password is too short')
+}
+else
+{
+    console.log('password is long enough')
+}
